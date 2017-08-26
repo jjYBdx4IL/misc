ba58396e6ffb480ac965115c97923a8e43f45105
jjYBdx4IL <jjYBdx4IL@github.com>
2017-08-26 20:30:12 +0200

    fix header

--- config-utils/src/test/java/com/github/jjYBdx4IL/utils/cfg/AbstractConfigTest.java
+++ config-utils/src/test/java/com/github/jjYBdx4IL/utils/cfg/AbstractConfigTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2014 jjYBdx4IL (https://github.com/jjYBdx4IL)
+ * Copyright © 2017 jjYBdx4IL (https://github.com/jjYBdx4IL)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
