6837108adf75237e6f1bf21beeee07a3dbc8f657
jjYBdx4IL <jjYBdx4IL@github.com>
2017-10-25 09:51:34 +0200

    working markdown conversion with line pointers

---
+++ text-utils/src/main/java/com/github/jjYBdx4IL/utils/markdown/MyMD.java
@@ -0,0 +1,124 @@
+package com.github.jjYBdx4IL.utils.markdown;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import org.apache.commons.text.StringEscapeUtils;
+
+//CHECKSTYLE:OFF
+/**
+ * Markdown to html convertor that allows editing of source parts.
+ * 
+ * <p>
+ * This class allows to edit markdown source of rendered html in-place:
+ * <ul>
+ * <li>md to html conversion, embedding of markdown source positions in html tag
+ * attributes
+ * <li>source positions can be used to retrieve markdown source parts for
+ * rendered html
+ * <li>source positions can be used to update source parts of rendered html
+ * </ul>
+ */
+public class MyMD {
+
+    public static final String EOL = "\n";
+
+    public static String html(String input) {
+        checkNotNull(input);
+        StringBuilder sb = new StringBuilder(input.length());
+        StringBuilder buf = new StringBuilder();
+        int startLine = -1;
+        State state = State.DEFAULT;
+        String[] lines = input.split("\r?\n");
+        for (int lineNum = 0; lineNum <= lines.length; lineNum++) {
+            String l = lineNum < lines.length ? lines[lineNum] : "";
+            switch (state) {
+                case DEFAULT:
+                    if (l.equals("```")) {
+                        state = State.PRE;
+                        startLine = lineNum;
+                        buf.setLength(0);
+                    } else if (l.startsWith("    ")) {
+                        state = State.IPRE;
+                        startLine = lineNum;
+                        buf.setLength(0);
+                        buf.append(l.substring(4));
+                    } else if (l.startsWith("> ")) {
+                        sb.append(String.format("<blockquote srcLines=\"%d-%d\">", lineNum, lineNum));
+                        sb.append(format(l.substring(2).trim()));
+                        sb.append("</blockquote>");
+                    } else if (l.startsWith("# ")) {
+                        sb.append(String.format("<h1 srcLines=\"%d-%d\">", lineNum, lineNum));
+                        sb.append(format(l.substring(2).trim()));
+                        sb.append("</h1>");
+                    } else if (l.startsWith("## ")) {
+                        sb.append(String.format("<h2 srcLines=\"%d-%d\">", lineNum, lineNum));
+                        sb.append(format(l.substring(3).trim()));
+                        sb.append("</h2>");
+                    } else if (l.startsWith("### ")) {
+                        sb.append(String.format("<h3 srcLines=\"%d-%d\">", lineNum, lineNum));
+                        sb.append(format(l.substring(4).trim()));
+                        sb.append("</h3>");
+                    } else if (l.startsWith("#### ")) {
+                        sb.append(String.format("<h4 srcLines=\"%d-%d\">", lineNum, lineNum));
+                        sb.append(format(l.substring(5).trim()));
+                        sb.append("</h4>");
+                    } else if (l.startsWith("##### ")) {
+                        sb.append(String.format("<h5 srcLines=\"%d-%d\">", lineNum, lineNum));
+                        sb.append(format(l.substring(6).trim()));
+                        sb.append("</h5>");
+                    } else if (l.startsWith("###### ")) {
+                        sb.append(String.format("<h6 srcLines=\"%d-%d\">", lineNum, lineNum));
+                        sb.append(format(l.substring(7).trim()));
+                        sb.append("</h6>");
+                    } else if (!l.isEmpty()) {
+                        sb.append(String.format("<p srcLines=\"%d-%d\">", lineNum, lineNum));
+                        sb.append(format(l.trim()));
+                        sb.append("</p>");
+                    }
+                    break;
+                case PRE:
+                    if (l.equals("```")) {
+                        state = State.DEFAULT;
+                        sb.append(String.format("<pre srcLines=\"%d-%d\">%s</pre>", startLine, lineNum,
+                            StringEscapeUtils.escapeHtml4(buf.toString())));
+                    } else {
+                        if (buf.length() > 0) {
+                            buf.append(EOL);
+                        }
+                        buf.append(l);
+                    }
+                    break;
+                case IPRE:
+                    if (l.startsWith("    ")) {
+                        buf.append(EOL);
+                        buf.append(l.substring(4));
+                    } else {
+                        state = State.DEFAULT;
+                        lineNum--;
+                        sb.append(String.format("<pre srcLines=\"%d-%d\">%s</pre>", startLine, lineNum,
+                            StringEscapeUtils.escapeHtml4(buf.toString())));
+                    }
+                    break;
+                default:
+
+            }
+        }
+        return sb.toString();
+    }
+
+    private static String format(String input) {
+        input = StringEscapeUtils.escapeHtml4(input);
+        input = input.replaceAll("\\[([^]]+)\\]\\(([^\")]+)\\)", "<a href=\"$2\">$1</a>");
+        input = input.replaceAll("\\*\\*\\*(.*?)\\*\\*\\*", "<b><i>$1</i></b>");
+        input = input.replaceAll("\\*\\*(.*?)\\*\\*", "<b>$1</b>");
+        input = input.replaceAll("\\*(.*?)\\*", "<i>$1</i>");
+        input = input.replaceAll("_(.*?)_", "<u>$1</u>");
+        input = input.replaceAll("`(.*?)`", "<code>$1</code>");
+        return input;
+    }
+
+    private static enum State {
+        DEFAULT, PRE, IPRE;
+    }
+
+}
---
+++ text-utils/src/test/java/com/github/jjYBdx4IL/utils/markdown/MyMDTest.java
@@ -0,0 +1,122 @@
+package com.github.jjYBdx4IL.utils.markdown;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MyMDTest {
+
+    @Test
+    public void testEmpty() {
+        assertEquals("", MyMD.html(""));
+    }
+
+    @Test
+    public void testParagraph() {
+        assertEquals("<p srcLines=\"0-0\">one</p>", MyMD.html("one"));
+    }
+
+    @Test
+    public void testParagraphEscape() {
+        assertEquals("<p srcLines=\"0-0\">one&lt;</p>", MyMD.html("one<"));
+    }
+
+    @Test
+    public void testHeadlines() {
+        assertEquals("<h1 srcLines=\"0-0\">one</h1>", MyMD.html("# one"));
+        assertEquals("<h2 srcLines=\"0-0\">one</h2>", MyMD.html("## one"));
+        assertEquals("<h3 srcLines=\"0-0\">one</h3>", MyMD.html("### one"));
+        assertEquals("<h4 srcLines=\"0-0\">one</h4>", MyMD.html("#### one"));
+        assertEquals("<h5 srcLines=\"0-0\">one</h5>", MyMD.html("##### one"));
+        assertEquals("<h6 srcLines=\"0-0\">one</h6>", MyMD.html("###### one"));
+    }
+
+    @Test
+    public void testHeadlinesEscape() {
+        assertEquals("<h1 srcLines=\"0-0\">one&lt;</h1>", MyMD.html("# one<"));
+        assertEquals("<h2 srcLines=\"0-0\">one&lt;</h2>", MyMD.html("## one<"));
+        assertEquals("<h3 srcLines=\"0-0\">one&lt;</h3>", MyMD.html("### one<"));
+        assertEquals("<h4 srcLines=\"0-0\">one&lt;</h4>", MyMD.html("#### one<"));
+        assertEquals("<h5 srcLines=\"0-0\">one&lt;</h5>", MyMD.html("##### one<"));
+        assertEquals("<h6 srcLines=\"0-0\">one&lt;</h6>", MyMD.html("###### one<"));
+    }
+
+    @Test
+    public void testFormatting() {
+        assertEquals("<p srcLines=\"0-0\"><i>one</i> <b>two</b> <b><i>three</i></b> <u>four</u> <i>five</i></p>",
+            MyMD.html("*one* **two** ***three*** _four_ *five*"));
+        assertEquals("<p srcLines=\"0-0\"><code>one</code></p>", MyMD.html("`one`"));
+    }
+
+    @Test
+    public void testFormattingEscape() {
+        assertEquals("<p srcLines=\"0-0\"><i>one&lt;</i> <b>two&lt;</b> <b><i>three&lt;</i></b> <u>four&lt;</u></p>",
+            MyMD.html("*one<* **two<** ***three<*** _four<_"));
+        assertEquals("<p srcLines=\"0-0\"><code>one&lt;</code></p>", MyMD.html("`one<`"));
+    }
+
+    @Test
+    public void testBlockquote() {
+        assertEquals("<blockquote srcLines=\"0-0\">one</blockquote>", MyMD.html("> one"));
+    }
+
+    @Test
+    public void testBlockquoteEscape() {
+        assertEquals("<blockquote srcLines=\"0-0\">one&lt;</blockquote>", MyMD.html("> one<"));
+    }
+
+    @Test
+    public void testIndentedPre() {
+        assertEquals("<pre srcLines=\"0-0\">one</pre>", MyMD.html("    one"));
+    }
+
+    @Test
+    public void testIndentedPreEscape() {
+        assertEquals("<pre srcLines=\"0-0\">one&lt;</pre>", MyMD.html("    one<"));
+    }
+
+    @Test
+    public void testPre() {
+        assertEquals("<pre srcLines=\"0-2\">one</pre>", MyMD.html("```\none\n```"));
+    }
+
+    @Test
+    public void testPreEscape() {
+        assertEquals("<pre srcLines=\"0-2\">one&lt;</pre>", MyMD.html("```\none<\n```"));
+    }
+
+    @Test
+    public void testLink() {
+        assertEquals("<p srcLines=\"0-0\"><a href=\"two\">one</a></p>", MyMD.html("[one](two)"));
+    }
+
+    @Test
+    public void testLinkEscape() {
+        assertEquals("<p srcLines=\"0-0\"><a href=\"two\">one&lt;</a></p>", MyMD.html("[one<](two)"));
+    }
+
+    @Test
+    public void testFullExample() {
+        String input = "# title\n"
+            + "The *first* **paragraph of** this ***simple te***xt.\n"
+            + "And the `next one`, and some par`ti`al code plus some [link](http://host).\n"
+            + "> The *first* **blockquote of** this ***simple te***xt.\n"
+            + "> And the `next one`, and some par`ti`al code plus some [link](http://host).\n"
+            + "    and now some <pre-formatted\n"
+            + "     text.\n"
+            + "```\n"
+            + "some other preformatted text.<\n"
+            + "  second line\n"
+            + "```";
+        String output = "<h1 srcLines=\"0-0\">title</h1>"
+            + "<p srcLines=\"1-1\">The <i>first</i> <b>paragraph of</b> this <b><i>simple te</i></b>xt.</p>"
+            + "<p srcLines=\"2-2\">And the <code>next one</code>, and some par<code>ti</code>al code plus"
+            + " some <a href=\"http://host\">link</a>.</p>"
+            + "<blockquote srcLines=\"3-3\">The <i>first</i> <b>blockquote of</b> this <b><i>simple te</i></b>xt.</blockquote>"
+            + "<blockquote srcLines=\"4-4\">And the <code>next one</code>, and some par<code>ti</code>al code plus"
+            + " some <a href=\"http://host\">link</a>.</blockquote>"
+            + "<pre srcLines=\"5-6\">and now some &lt;pre-formatted\n text.</pre>"
+            + "<pre srcLines=\"7-10\">some other preformatted text.&lt;\n  second line</pre>";
+        assertEquals(output, MyMD.html(input));
+    }
+}
