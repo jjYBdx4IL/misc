2cbf39626b5211c63e5065cb6568778eaa3b7d83
jjYBdx4IL <jjYBdx4IL@github.com>
2019-02-12 21:48:15 +0100

    add interceptor for exceptions

---
+++ cms-webapp/src/main/java/com/github/jjYBdx4IL/cms/rest/app/ExceptionInterceptor.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright © 2017 jjYBdx4IL (https://github.com/jjYBdx4IL)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.jjYBdx4IL.cms.rest.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class ExceptionInterceptor implements ExceptionMapper<Throwable> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ExceptionInterceptor.class);
+    
+    @Override
+    public Response toResponse(Throwable exception) {
+        if (exception instanceof NotFoundException) {
+            return Response.status(Status.NOT_FOUND).build();
+        }
+        LOG.error("", exception);
+        return Response.serverError().build();
+    }
+    
+}
