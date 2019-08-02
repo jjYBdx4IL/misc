0fe9b3a677ddf963e0636a02b476d0d365d9ebfc
jjYBdx4IL <jjYBdx4IL@github.com>
2019-08-02 20:29:44 +0200

    add linux cpu time usage output parser

---
+++ parser/src/main/java/com/github/jjYBdx4IL/parser/linux/CpuTimeOutputParser.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright © 2017 jjYBdx4IL (https://github.com/jjYBdx4IL)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.jjYBdx4IL.parser.linux;
+
+import com.github.jjYBdx4IL.parser.ParseException;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Parses and analyzes "time" command output, which is available on linux boxes.
+ *
+ */
+public class CpuTimeOutputParser {
+
+    public static final Pattern PAT = Pattern.compile("(real|user|sys)\\s+(\\d+)m(\\d+)[,.](\\d+)s");
+
+    /**
+     * All values are stored as milliseconds.
+     *
+     */
+    public static class UsageData {
+        final long real;
+        final long user;
+        final long sys;
+
+        UsageData(long real, long user, long sys) {
+            this.real = real;
+            this.user = user;
+            this.sys = sys;
+        }
+
+        public String toString() {
+            return "UsageData [real=" + real + "ms, user=" + user + "ms, sys=" + sys + "ms]";
+        }
+    }
+
+    /**
+     * Parses and analyzes "time" command output, which is available on linux
+     * boxes. Takes a single output in the form:
+     * 
+     * <pre>
+     * real 0m37,766s
+     * user 1m32,058s
+     * sys  0m2,724s
+     * </pre>
+     * 
+     * @param input
+     *            the string to parse
+     * @return the parsed result
+     * @throws ParseException
+     *             on error
+     */
+    public static UsageData parse(String input) throws ParseException {
+        Matcher m = PAT.matcher(input);
+        Long real = null;
+        Long user = null;
+        Long sys = null;
+        int count = 0;
+        while (m.find()) {
+            String key = m.group(1);
+            Long minutes = Long.parseLong(m.group(2));
+            Long seconds = Long.parseLong(m.group(3));
+            String msStr = m.group(4);
+            while (msStr.length() < 3) {
+                msStr += "0";
+            }
+            long millis = ((minutes * 60) + seconds) * 1000L + Long.parseLong(msStr);
+            if ("real".equals(key)) {
+                real = millis;
+            } else if ("user".equals(key)) {
+                user = millis;
+            } else if ("sys".equals(key)) {
+                sys = millis;
+            } else {
+                throw new ParseException("invalid format: " + input);
+            }
+            count++;
+        }
+        if (count != 3 || real == null || user == null || sys == null) {
+            throw new ParseException("invalid format: " + input);
+        }
+        return new UsageData(real, user, sys);
+    }
+
+    /**
+     * Same as {@link #parse(String)}, but the input string may consist of
+     * multiple outputs.
+     * 
+     * @param input
+     *            the string to parse
+     * @return the parsed result
+     * @throws ParseException
+     *             on error
+     */
+    public static List<UsageData> parseMulti(String input) throws ParseException {
+        List<UsageData> result = new ArrayList<>();
+        String[] parts = input.split("real");
+        boolean first = true;
+        for (String part : parts) {
+            if (first) {
+                first = false;
+                continue;
+            }
+            result.add(parse("real" + part));
+        }
+        return result;
+    }
+    
+    /**
+     * Calculate mean and standard deviations for a list of UsageData.
+     * 
+     * @param data minimu length is 2
+     * @return the result
+     */
+    public static String dumpStats(List<UsageData> data) {
+        if (data.size() < 2) {
+            throw new IllegalArgumentException("list too short, min length is 2");
+        }
+        
+        long realSum = 0;
+        long userSum = 0;
+        long sysSum = 0;
+        for (UsageData usage : data) {
+            realSum += usage.real;
+            userSum += usage.user;
+            sysSum += usage.sys;
+        }
+        double realMean = 1d * realSum / data.size();
+        double userMean = 1d * userSum / data.size();
+        double sysMean = 1d * sysSum / data.size();
+        double realSqDeltaSum = 0d;
+        double userSqDeltaSum = 0d;
+        double sysSqDeltaSum = 0d;
+        for (UsageData usage : data) {
+            realSqDeltaSum += Math.pow(usage.real - realMean, 2);
+            userSqDeltaSum += Math.pow(usage.user - userMean, 2);
+            sysSqDeltaSum += Math.pow(usage.sys - sysMean, 2);
+        }
+        double realSdev = Math.sqrt(realSqDeltaSum) / (data.size() - 1);
+        double userSdev = Math.sqrt(userSqDeltaSum) / (data.size() - 1);
+        double sysSdev = Math.sqrt(sysSqDeltaSum) / (data.size() - 1);
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("real: (%,.0f +/- %,.0f)ms %s", realMean, realSdev, System.lineSeparator()));
+        sb.append(String.format("user: (%,.0f +/- %,.0f)ms %s", userMean, userSdev, System.lineSeparator()));
+        sb.append(String.format("sys: (%,.0f +/- %,.0f)ms", sysMean, sysSdev, System.lineSeparator()));
+        return sb.toString();
+    }
+    
+    /**
+     * Run this file to calculate statistics from the pasted cpu time usage outputs.
+     * 
+     * @param args not used
+     * @throws IOException on error
+     * @throws ParseException on error
+     */
+    public static void main(String[] args) throws IOException, ParseException {
+        StringBuilder sb = new StringBuilder();
+        System.out.println("Paste to text to parse and press CTRL-D:");
+        String input = IOUtils.toString(System.in, Charset.defaultCharset());
+        List<UsageData> result = parseMulti(input);
+        System.out.println(dumpStats(result));
+    }
+}
---
+++ parser/src/test/java/com/github/jjYBdx4IL/parser/linux/CpuTimeOutputParserTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright © 2017 jjYBdx4IL (https://github.com/jjYBdx4IL)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.jjYBdx4IL.parser.linux;
+
+import static org.junit.Assert.assertEquals;
+
+import com.github.jjYBdx4IL.parser.ParseException;
+import com.github.jjYBdx4IL.parser.linux.CpuTimeOutputParser.UsageData;
+import org.junit.Test;
+
+import java.util.List;
+
+public class CpuTimeOutputParserTest {
+
+    @Test
+    public void testParse() throws ParseException {
+        assertEquals("UsageData [real=38197ms, user=98938ms, sys=2804ms]", CpuTimeOutputParser.parse(
+            "real    0m38,197s\n" + 
+            "user    1m38,938s\n" + 
+            "sys 0m2,804s").toString());
+    }
+    
+    @Test
+    public void testParseMulti() throws ParseException {
+        assertEquals(0, CpuTimeOutputParser.parseMulti("").size());
+        
+        List<UsageData> result = CpuTimeOutputParser.parseMulti(
+            "real    0m38,197s\n" + 
+            "user    1m38,938s\n" + 
+            "sys 0m2,804s");
+        assertEquals("UsageData [real=38197ms, user=98938ms, sys=2804ms]", result.get(0).toString());
+        
+        result = CpuTimeOutputParser.parseMulti(
+            "real    0m38,197s\n" + 
+            "user    1m38,938s\n" + 
+            "sys 0m2,804s" +
+            "real    0m38,198s\n" + 
+            "user    1m38,939s\n" + 
+            "sys 0m2,805s");
+        assertEquals("UsageData [real=38197ms, user=98938ms, sys=2804ms]", result.get(0).toString());
+        assertEquals("UsageData [real=38198ms, user=98939ms, sys=2805ms]", result.get(1).toString());
+    }
+    
+    @Test
+    public void testDumpStats() throws ParseException {
+        List<UsageData> result = CpuTimeOutputParser.parseMulti(
+            "real    0m1,0s\n" + 
+            "user    0m1,0s\n" + 
+            "sys 0m1,0s\n" +
+            "real    0m1,0s\n" + 
+            "user    0m2,0s\n" + 
+            "sys 0m1,0s");
+        assertEquals(
+            "real: (1.000 +/- 0)ms \n" + 
+            "user: (1.500 +/- 707)ms \n" + 
+            "sys: (1.000 +/- 0)ms",
+            CpuTimeOutputParser.dumpStats(result));
+    }
+}
